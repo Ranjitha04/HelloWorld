2a502eb379cac0a6f1998857c36311d89f1dba3b
Ranjitha R <ranjitha.madan@gmail.com>
2020-06-30 10:24:51 +0530

    added comment

--- src/main/java/com/example/HelloWorld/controller/WelcomeController.java
+++ src/main/java/com/example/HelloWorld/controller/WelcomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    /* the mapping gives welcome message*/
 	@GetMapping("/")
 	public String welcomeMessage() {
 		return "Welcome To Hellworld Application";
